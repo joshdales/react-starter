825db650a6018f650e4b33da2ec5cc7b235602a6
Josh Dales <git@joshdales.dev>
2023-06-23 16:55:33 -0400

    Found the types for pxToRem

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import autoprefixer from 'autoprefixer'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore - this package doesn't have any types :(
 import pxToRem from 'postcss-pxtorem'
 
 export default defineConfig({
