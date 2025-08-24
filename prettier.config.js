bd067276f188e7dadfaaf3dedc0bf658551d07bc
Josh Dales <git@joshdales.dev>
2025-08-24 07:33:15 -0400

    Update deps

--- prettier.config.js
+++ prettier.config.js
@@ -2,10 +2,12 @@
  * @type {import("prettier").Config}
  */
 const config = {
-	semi: false,
-	singleQuote: true,
-	jsxSingleQuote: false,
-	arrowParens: 'avoid',
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  arrowParens: 'avoid',
+  useTabs: true,
+  tabWidth: 2,
 }
 
 export default config
--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,29 @@ import react from '@vitejs/plugin-react'
 
 let base = process.env.REPO_NAME
 if (base && !base.startsWith('/')) {
-	base = `/${base}`
+  base = `/${base}`
 }
 
 export default defineConfig({
-	plugins: [
-		react({
-			babel: {
-				plugins: ['babel-plugin-react-compiler'],
-			},
-		}),
-	],
-	server: {
-		port: 3000,
-	},
-	css: {
-		transformer: 'lightningcss',
-		modules: {
-			localsConvention: 'camelCaseOnly',
-			generateScopedName: '[name]__[local]',
-		},
-	},
-	build: {
-		cssMinify: 'lightningcss',
-	},
-	base,
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+  css: {
+    transformer: 'lightningcss',
+    modules: {
+      localsConvention: 'camelCaseOnly',
+      generateScopedName: '[name]__[local]',
+    },
+  },
+  build: {
+    cssMinify: 'lightningcss',
+  },
+  base,
 })
