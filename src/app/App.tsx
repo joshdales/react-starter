8e5cbb348d08b519e14f0cbf70cb58e24e8603aa
Josh Dales <git@joshdales.dev>
2026-03-13 09:18:15 -0400

    Add components and app dirs

---
+++ src/app/App.tsx
@@ -0,0 +1,29 @@
+import { Footer } from '$components/Footer'
+import './App.css'
+
+export const App: React.FC = () => {
+	return (
+		<>
+			<main>
+				<h1 className="heading-5">This is a basic React app</h1>
+
+				<p className="body-4">
+					It's for quickly creating a Typescript based react app with settings
+					that I like.
+				</p>
+
+				<p className="body-4">
+					It doesn't do anything fancy, it's just an easy setup for getting
+					started with styles and some tools like eslint and prettier.
+				</p>
+
+				<p className="body-4">
+					And as an added bonus you can easily deploy it to Github Pages by
+					manually running the <strong>Deploy Project</strong> workflow.
+				</p>
+			</main>
+
+			<Footer />
+		</>
+	)
+}
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+export const Footer: FC = () => {
+	return (
+		<footer>
+			<p className="body-1">
+				Made with{' '}
+				<a className="link-1" href="https://github.com/joshdales/react-starter">
+					joshdales/react-starter
+				</a>{' '}
+				template
+			</p>
+		</footer>
+	)
+}
