7b9808b4599f4130c05783321310380a63208b69
Josh Dales <git@joshdales.dev>
2026-03-13 09:20:10 -0400

    Fix fmt

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,18 +8,17 @@ export const App: React.FC = () => {
 				<h1 className="heading-5">This is a basic React app</h1>
 
 				<p className="body-4">
-					It's for quickly creating a Typescript based react app with settings
-					that I like.
+					It's for quickly creating a Typescript based react app with settings that I like.
 				</p>
 
 				<p className="body-4">
-					It doesn't do anything fancy, it's just an easy setup for getting
-					started with styles and some tools like eslint and prettier.
+					It doesn't do anything fancy, it's just an easy setup for getting started with styles and
+					some tools like eslint and prettier.
 				</p>
 
 				<p className="body-4">
-					And as an added bonus you can easily deploy it to Github Pages by
-					manually running the <strong>Deploy Project</strong> workflow.
+					And as an added bonus you can easily deploy it to Github Pages by manually running the{' '}
+					<strong>Deploy Project</strong> workflow.
 				</p>
 			</main>
 
